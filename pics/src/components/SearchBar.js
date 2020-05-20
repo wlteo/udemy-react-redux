7696dda5abc0e886c5cda0adfd383a120075f815
wlteo <xeniel@gmail.com>
2020-05-20 23:52:57 +0800

    Customize search bar

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,18 @@
 import React from 'react'
 class SearchBar extends React.Component {
   state = { term: 'type something here bro' }
+  
   onInputChange(event) {
     console.log(event.target.value)
-  } 
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault()
+  }
 
   render() {
     return <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input type='text' value={this.state.term} 
